23da7dc10bf7e0c32fdeac29b8e27d03fe8cca18
rkpatel94 <rajkumar.patel1994@gmail.com>
2023-07-12 17:28:48 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,35 +1,44 @@
 import React from "react";
-import Graph from "react-graph-vis";
+import Graph from "react-vis-network-graph";
 import DIR from "../src/logo.svg";
+
+const nodeData = [
+  { id: 1, label: "Node 1", shape: "circularImage", image: DIR },
+  { id: 2, label: "Node 2", shape: "circularImage", image: DIR },
+  { id: 3, label: "Node 3", shape: "circularImage", image: DIR },
+  { id: 4, label: "Node 4", shape: "circularImage", image: DIR },
+  { id: 5, label: "Node 5", shape: "circularImage", image: DIR },
+  { id: 6, label: "Node 6", shape: "circularImage", image: DIR },
+  { id: 7, label: "Node 7", shape: "circularImage", image: DIR }
+]
+
+// console.log(network.getPositions("1"),"position");
+
+const edgeData = [
+  { from: 1, to: 2, label: "lkmlkkl" },
+  { from: 2, to: 3 },
+  { from: 2, to: 4 },
+  { from: 4, to: 5 },
+  { from: 5, to: 6 },
+  { from: 7, to: 5 }
+]
 
 function App() {
   const graph = {
-    nodes: [
-      { id: 1, label: "Node 1", shape: "circularImage",image: DIR + "1.png",title: "node 1 tooltip text" },
-      { id: 7, label: "Node 7", shape: "circularImage",image: DIR + "1.png",title: "node 7 tooltip text" },
-      { id: 2, label: "Node 2", shape: "circularImage",image: DIR + "1.png",title: "node 2 tooltip text" },
-      { id: 3, label: "Node 3", shape: "circularImage",image: DIR + "1.png",title: "node 3 tooltip text" },
-      { id: 4, label: "Node 4", shape: "circularImage",image: DIR + "1.png",title: "node 4 tooltip text" },
-      { id: 5, label: "Node 5", shape: "circularImage",image: DIR + "1.png",title: "node 5 tooltip text" },
-      { id: 6, label: "Node 6", shape: "circularImage",image: DIR + "1.png",title: "node 6 tooltip text" }
-    ],
-    edges: [
-      { from: 1, to: 2 },
-      { from: 2, to: 3 },
-      { from: 2, to: 4 },
-      { from: 4, to: 5 },
-      { from: 5, to: 6 },
-      { from: 7, to: 5 }
-    ]
-  };
+    nodes: nodeData,
+    edges: edgeData,
+    smooth: true,
+    arrows: {
+      shadow: true,
+      to: {
+        scaleFactor: 1,
+      },
+     
+    }
+  }
  
   const options = {
-    layout: {
-      hierarchical: true
-    },
-    edges: {
-      color: "#000000"
-    },
+    physics: {enabled:false},
     height: "500px"
   };
  
